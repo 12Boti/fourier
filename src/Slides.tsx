139f55e8e7791268c179c23b2fc225c8b9c9a815
Domivokoks <kovacs.dominik.b@gmail.com>
2023-03-09 15:45:11 +0100

    Sin függvény slide, képlet átírása

--- src/Slides.tsx
+++ src/Slides.tsx
@@ -31,12 +31,36 @@ const Slides: Component = () => {
     });
   })(); 
   onCleanup(() => { if (unsubscribe) unsubscribe() });
-
+  const points = () => linspace(-7*Math.PI, 7*Math.PI, 1000).map((x) => [x, Math.sin(x/Math.PI*440)]);
 
   return <>
     <section><h1>Fourier-transzformáció</h1></section>
     <section>
-      <AsciiMath>hat(f)(xi) = int_-oo^oo f(x)e^(-i2pi xi x)dx</AsciiMath>
+      <div class="absolute"> Ez itt:
+      </div>
+      <div class="grid-item"><AsciiMath>hat(g)(f) = int_-oo^oo g(t)e^(-i2pi f t)dt</AsciiMath>
+      </div>
+      
+    </section>
+    <section>
+      <AsciiMath>f(x)=sinx</AsciiMath>
+      <svg width="800" height="300" viewBox="-1.5 -5 20 7" >
+        <line x1="-30" y1="0" x2="30" y2="0" stroke-width="2" stroke="#E04C1F" vector-effect="non-scaling-stroke"></line>
+        <line x1="0" y1="30" x2="0" y2="-30" stroke-width="2" stroke="#E04C1F" vector-effect="non-scaling-stroke"></line>
+        <line x1="18" y1="0.5" x2="18.5" y2="0" stroke-width="2" stroke="#E04C1F" vector-effect="non-scaling-stroke"></line>
+        <line x1="18" y1="-0.5" x2="18.5" y2="0" stroke-width="2" stroke="#E04C1F" vector-effect="non-scaling-stroke"></line>
+        <line x1="0" y1="-5.25" x2="0.5" y2="-4.75" stroke-width="2" stroke="#E04C1F" vector-effect="non-scaling-stroke"></line>
+        <line x1="0" y1="-5.25" x2="-0.5" y2="-4.75" stroke-width="2" stroke="#E04C1F" vector-effect="non-scaling-stroke"></line>
+        <text x="-5%" y="-60%" fill="#d02fa0" font-size="1" vector-effect="non-scaling-stroke"> Δ
+        </text>
+        
+      <polyline
+        stroke={"#1FB3E0"} stroke-width="5" fill="none" vector-effect="non-scaling-stroke"
+        points={points().map(([x, y]) => `${x} ${-y}`).join(",")} />
+      <text x="85%" y="-15%" font-size="1" fill="#d02fa0" vector-effect="non-scaling-stroke"> t(s)
+        </text>
+    </svg>
+    
     </section>
     <section on:reveal={anim.start}>
       <div class="flex items-center justify-center">
