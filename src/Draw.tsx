eaf3ca0e118c0dbd8d7f9c68da1b91a848e5ecab
unknown <furkasz42@gmail.com>
2023-03-09 15:20:08 +0100

    zoom

--- src/Draw.tsx
+++ src/Draw.tsx
@@ -7,12 +7,14 @@
 
 
 
-import { Component, createSignal, For, createMemo } from 'solid-js';
+import { Component, createSignal, For, createMemo, createEffect, on, startTransition, useTransition } from 'solid-js';
 import {Complex} from 'complex.js';
 import { normalized, fft } from './fourier';
 import { Polyline, Svg } from './Svg';
 import { createPointerListeners } from "@solid-primitives/pointer";
 import { createRAF } from '@solid-primitives/raf';
+import createTween from '@solid-primitives/tween';
+import { createTweenedComplex, createTweenedNumber } from './animation';
 
 function zip<A, B>(a: A[], b: B[]): [A, B][] {
   return a.map((k, i) => [k, b[i]]);
@@ -23,28 +25,56 @@ function floorPowerOfTwo(n: number): number {
 }
 
 const Draw: Component = () => {
+  let zoomed = false;
+  const [zoom, setZoom] = createTweenedNumber(1, {duration: 50000});
+  const complexZoom = () => Complex(zoom(), zoom());
+  const [camera, setCamera] = createSignal(Complex(0, 0));
   const [drawing, setDrawing] = createSignal<Complex[]>([]);
   const [fourier, setFourier] = createSignal<{z: Complex, freq: number}[]>([]);
   let startTime = 0;
   const [currentTime, setCurrentTime] = createSignal(0);
-  const time = () => Math.floor((currentTime() - startTime)/100)*2*Math.PI/fourier().length;
-  const offsets = createMemo(() => fourier().map(({z, freq}) => Complex({abs: z.abs(), arg: freq * time() + z.arg()})));
+  const time = () => Math.floor((currentTime() - startTime)/10)*2*Math.PI/(fourier().length);
+  const filteredFourier = () => fourier().filter(({z})=> z.abs() > 0.001);
+  const offsets = createMemo(() => filteredFourier().map(({z, freq}) => Complex({abs: z.abs(), arg: freq * time() + z.arg()})));
   const positions = () => {
     const o = offsets();
     const a = Array(o.length+1);
     a[0] = Complex.ZERO;
-    for (let i = 1; i < o.length; i++) {
+    for (let i = 1; i < o.length+1; i++) {
         a[i] = a[i-1].add(o[i-1]);
     }
     return a;
   };
+  const lastPosition = () => positions()[positions().length-1];
+  createEffect(on([positions], () => {
+      setDrawing([...drawing().slice(-fourier().length*fourier().length), lastPosition()]);
+      if(zoomed) {
+        setCamera(lastPosition());
+      }
+  }));
   let svg: SVGSVGElement;
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'z') {
+      if(!zoomed){
+        zoomed = true;
+        setZoom(0.003);
+      }
+      else {
+        setCamera(Complex(0,0));
+        zoomed = false;
+        setZoom(1);
+      }
+    }
+  };
+
 
   createPointerListeners({
     target: () => svg,
     ondown: _ => {
       setFourier([]);
       setDrawing([]);
+      setZoom(1);
     },
     onmove: e => {
       if (e.buttons == 0) return;
@@ -54,14 +84,27 @@ const Draw: Component = () => {
       setDrawing([...drawing(), p]);
     },
     onup: _ => {
-      const l = drawing().length;
+      let francium = drawing();
+      for(let j = 0; j < 5; j++) {
+        let neptunium = [];
+        for(let i = 0; i < francium.length; i++) {
+          neptunium.push(francium[i]);
+          if(francium.length > i+1) {
+            neptunium.push(francium[i].add(francium[i+1]).div(2));
+          }
+        }
+        francium = neptunium;
+      }
+      const l = francium.length;
       const diff = l-floorPowerOfTwo(l);
       const step = Math.floor(l/diff);
-      const signallium = drawing().filter((_, k) => k%step !== 0 || k>diff*step);
-      const f = normalized(fft(signallium)).map((z, freq) => ({z, freq}));
+      const signallium = francium.filter((_, k) => (k+1)%step !== 0 || k>diff*step);
+      //console.log(signallium);
+      const f = normalized(fft(signallium.concat(signallium.slice().reverse()))).map((z, freq) => ({z, freq}));
       f.sort((a, b) => b.z.abs() - a.z.abs());
-      console.log(f);
+      //console.log(f);
       setFourier(f);
+      setDrawing([]);
       startTime = currentTime();
     },
   });
@@ -71,14 +114,14 @@ const Draw: Component = () => {
   //createEffect(() => console.log(time()));
 
   return (
-    <Svg ref={svg} min={Complex(-1, -1)} max={Complex(1, 1)} class="w-full h-full">
-      <For each={zip(fourier(), positions())}>
+    <Svg ref={svg} onKeyDown={handleKeyDown}  min={camera().sub(complexZoom())} max={camera().add(complexZoom())} class="w-full h-full" tabindex="0">
+      <For each={zip(filteredFourier(), positions())}>
         {([{z, freq}, p]) => <>
-          <circle cx={p.re} cy={-p.im} r={z.abs()} fill="none" stroke="#FFA5FA77" stroke-width="0.003"></circle>
+          <circle cx={p.re} cy={-p.im} r={z.abs()} fill="none" stroke="#FFA5FA77" stroke-width="1" vector-effect="non-scaling-stroke"></circle>
         </>}
       </For>
-      <Polyline points={drawing()} stroke-width="0.003" />
-      <Polyline points={positions()} stroke-width="0.003" stroke="#FCFC05" />
+      <Polyline points={drawing()} stroke-width="1" vector-effect="non-scaling-stroke" />
+      <Polyline points={positions()} stroke-width="1" vector-effect="non-scaling-stroke" stroke="#FCFC05" />
     </Svg>
   );
 };
